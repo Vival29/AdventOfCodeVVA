ce16660f960249d1647fb07190885d640bcab65c
Vival29 <virginie.f.r.valentin@gmail.com>
2023-05-13 17:16:52 +0200

    Day 2 done

--- src/main/java/hearc/ig/Main.java
+++ src/main/java/hearc/ig/Main.java
@@ -1,6 +1,7 @@
 package hearc.ig;
 
 import hearc.ig.day1.Day1;
+import hearc.ig.day2.Day2;
 import hearc.ig.day3.Day3;
 import hearc.ig.day4.Day4;
 
@@ -9,22 +10,29 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Day1 day1 = new Day1();
+        Day2 day2 = new Day2();
         Day3 day3 = new Day3();
         Day4 day4 = new Day4();
         Persistance data = new Persistance();
 
-        List<String> calories = data.getInputByLine(day1.PATH);
         System.out.println("Day1");
+        List<String> calories = data.getInputByLine(day1.PATH);
         System.out.println(day1.resolvePuzzlePart1(calories));
-        System.out.println(day1.resolvePuzzlePart2());
+        System.out.println(day1.resolvePuzzlePart2() + "\n");
+
+        System.out.println("Day2");
+        List<String> tournament = data.getInputByLine(day2.PATH);
+        System.out.println(day2.resolvePuzzlePart1(tournament));
+        System.out.println(day2.resolvePuzzlePart2(tournament) + "\n");
+
         System.out.println("Day3");
         List<String> sacs = data.getInputByLine(day3.PATH);
         System.out.println(day3.resolvePuzzlePart1(sacs));
-        System.out.println(day3.resolvePuzzlePart2(sacs));
+        System.out.println(day3.resolvePuzzlePart2(sacs)+ "\n");
         System.out.println("Day4");
         List<String> pairs = data.getInputByLine(day4.PATH);
         System.out.println(day4.resolvePuzzlePart1(pairs));
-        System.out.println(day4.resolvePuzzlePart2(pairs));
+        System.out.println(day4.resolvePuzzlePart2(pairs)+ "\n");
 
 
     }
--- src/main/java/hearc/ig/day2/Day2.java
+++ src/main/java/hearc/ig/day2/Day2.java
@@ -1,4 +1,118 @@
 package hearc.ig.day2;
 
+import hearc.ig.day1.Day1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
 public class Day2 {
+    private static final Logger logger = LoggerFactory.getLogger(Day1.class);
+    public static final String PATH = "src/main/java/hearc/ig/day2/inputDay2.txt";
+
+    public Integer resolvePuzzlePart1(List<String> tournament) {
+        Integer score = 0;
+        for (String match : tournament) {
+            char[] hand = match.toCharArray();
+            char opponentHand = hand[0];
+            char myHand = hand[2];
+
+            switch (opponentHand) {
+                case 'A':
+                    switch (myHand) {
+                        case 'X':
+                            score = score + 1 + 3;
+                            break;
+                        case 'Y':
+                            score = score + 2 + 6;
+                            break;
+                        case 'Z':
+                            score = score + 3 + 0;
+                            break;
+                    }
+                    break;
+                case 'B':
+                    switch (myHand) {
+                        case 'X':
+                            score = score + 1 + 0;
+                            break;
+                        case 'Y':
+                            score = score + 2 + 3;
+                            break;
+                        case 'Z':
+                            score = score + 3 + 6;
+                            break;
+                    }
+                    break;
+                case 'C':
+                    switch (myHand) {
+                        case 'X':
+                            score = score + 1 + 6;
+                            break;
+                        case 'Y':
+                            score = score + 2 + 0;
+                            break;
+                        case 'Z':
+                            score = score + 3 + 3;
+                            break;
+                    }
+                    break;
+            }
+        }
+        return score;
+    }
+
+    public Integer resolvePuzzlePart2(List<String> tournament) {
+        Integer realscore=0;
+        for (String match : tournament) {
+            char[] hand = match.toCharArray();
+            char opponentHand = hand[0];
+            char myHand = hand[2];
+
+            switch (opponentHand) {
+                case 'A':
+                    switch (myHand) {
+                        case 'X':
+                            realscore = realscore + 3 + 0;
+                            break;
+                        case 'Y':
+                            realscore = realscore + 1 + 3;
+                            break;
+                        case 'Z':
+                            realscore = realscore + 2 + 6;
+                            break;
+                    }
+                    break;
+                case 'B':
+                    switch (myHand) {
+                        case 'X':
+                            realscore = realscore + 1 + 0;
+                            break;
+                        case 'Y':
+                            realscore = realscore + 2 + 3;
+                            break;
+                        case 'Z':
+                            realscore = realscore + 3 + 6;
+                            break;
+                    }
+                    break;
+                case 'C':
+                    switch (myHand) {
+                        case 'X':
+                            realscore = realscore + 2 + 0;
+                            break;
+                        case 'Y':
+                            realscore = realscore + 3 + 3;
+                            break;
+                        case 'Z':
+                            realscore = realscore + 1 + 6;
+                            break;
+                    }
+                    break;
+            }
+        }
+        return realscore;
+    }
 }
+
+
